11939fa1fc003abff570d7a33db2f8b75b9fe5f4
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-06-30 19:31:56 -0500

    Create a file to seed database, create a comment model and display it in detailed page correctly

---
+++ seedDatabase.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const TravelClub = require('./models/travelclub');
+const Comment = require('./models/comment');
+
+const travelPlacesArray = [
+  {
+    name: 'Austin',
+    image: 'http://cdn.destination360.com/north-america/us/texas/images/s/austin-day-trip.jpg',
+    description: 'Mount Bonnell is the peak within Covert Park located on Mount Bonnell Drive and offers public access to panoramic views several hundred feet above Lake Austin'
+  },
+  {
+    name: 'Austin',
+    image: 'http://cdn.destination360.com/north-america/us/texas/images/s/austin-day-trip.jpg',
+    description: 'Mount Bonnell is the peak within Covert Park located on Mount Bonnell Drive and offers public access to panoramic views several hundred feet above Lake Austin'
+  },
+  {
+    name: 'Austin',
+    image: 'http://cdn.destination360.com/north-america/us/texas/images/s/austin-day-trip.jpg',
+    description: 'Mount Bonnell is the peak within Covert Park located on Mount Bonnell Drive and offers public access to panoramic views several hundred feet above Lake Austin'
+  }
+];
+
+const SeedDatabase = () => {
+  // Remove all travel places from DB
+  TravelClub.remove({}, (err) => {
+    if(err) console.log(err);
+    else {
+      console.log('All travel places are removed!');
+      // Once the data is removed
+      // Then Add a travel place(s)
+      travelPlacesArray.map(travelPlace => {
+        TravelClub.create(travelPlace, (err, travelPlace) => {
+          if(err) console.log(err);
+          else {
+            Comment.create({ text: 'This place is beautiful', author: 'John Doe' }, (err, comment) => {
+              if(err) console.log(err);
+              travelPlace.comments.push(comment);
+              travelPlace.save();
+              console.log('New comment is created');
+            });
+          }
+        });
+      });
+    }
+  });
+}
+
+module.exports = SeedDatabase;
+
