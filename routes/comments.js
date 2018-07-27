fdb66ff1762388fc22a6df469a68910a89870fab
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-07-27 07:52:23 -0500

    Redirect user back to comment page when a comment is deleted, change delete botton's position and switch id to class to use the same styling

--- routes/comments.js
+++ routes/comments.js
@@ -75,13 +75,13 @@ router.put('/:comment_id', (req, res) => {
 
 // Destroy Route - Delete Comments
 router.delete('/:comment_id', (req, res) => {
-  const { comment_id } = req.params;
+  const { id, comment_id } = req.params;
   Comment.findByIdAndRemove(comment_id, (err) => {
     if(err) {
       res.redirect('back');
       console.log(err);
     } else {
-      res.redirect('/travelplaces');
+      res.redirect(`/travelplaces/${id}`);
     }
   });
 });
