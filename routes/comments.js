09b65367e6770058b52410fbede5b99d2dd7a34e
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-07-27 09:08:34 -0500

    Implement comment authorization by creating a middleware that checks the ownership to edit or delete a comment

--- routes/comments.js
+++ routes/comments.js
@@ -3,10 +3,33 @@ const router = express.Router({ mergeParams: true }); //mergeParams enables us t
 const TravelClub = require('../models/travelclub');
 const Comment = require('../models/comment');
 
-// Create middleware to handle unathorized access
+// Create a middleware to handle unathorized access
 const isLoggedIn = (req, res, next) => {
   if(req.isAuthenticated()) return next(); // if authorized, move to next step
   res.redirect('/login'); // if not authorized, redirect to login
+}
+
+// middleware to check authorization
+const checkCommentOwnership = (req, res, next) => {
+  const { comment_id } = req.params;
+  // if user is logged in
+  if (req.isAuthenticated()) {
+    Comment.findById(comment_id, (err, foundComment) => {
+      if(err) {
+        res.redirect('back');  // using back takes user back to the original page
+        console.log(err);
+      } else {
+        // does user own the travel place?
+        if (foundComment.author.id.equals(req.user._id)) {  // we need to use mongoose' equals method bc author.id is an object
+          next();                                           // and req.user._id is a string even though they look identical
+        } else { // otherwise redirect
+          res.redirect('back');
+        }
+      }
+    });
+  } else { // user is not logged in so redirect back
+    res.redirect('back'); 
+  }
 }
 
 // New Comments
@@ -47,7 +70,7 @@ router.post('/', isLoggedIn, (req, res) => {
 });
 
 // Edit Comments
-router.get('/:comment_id/edit', (req, res) => {
+router.get('/:comment_id/edit', checkCommentOwnership, (req, res) => {
   const { id, comment_id } = req.params;
   Comment.findById(comment_id, (err, foundComment) => {
     if(err) {
@@ -60,7 +83,7 @@ router.get('/:comment_id/edit', (req, res) => {
 });
 
 // Update Comments
-router.put('/:comment_id', (req, res) => {
+router.put('/:comment_id', checkCommentOwnership, (req, res) => {
   const { id, comment_id } = req.params;
   const { comment } = req.body;
   Comment.findByIdAndUpdate(comment_id, comment, (err, updatedComment) => {
@@ -74,7 +97,7 @@ router.put('/:comment_id', (req, res) => {
 });
 
 // Destroy Route - Delete Comments
-router.delete('/:comment_id', (req, res) => {
+router.delete('/:comment_id', checkCommentOwnership, (req, res) => {
   const { id, comment_id } = req.params;
   Comment.findByIdAndRemove(comment_id, (err) => {
     if(err) {
